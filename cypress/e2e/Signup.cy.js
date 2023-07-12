8ecfd585e2dc8b4f07452a70952c1491161a3f24
bunmeey <idowuoluwabunmeey@gmail.com>
2023-07-12 20:15:45 +0100

    Added yaml file

--- cypress/e2e/Signup.cy.js
+++ cypress/e2e/Signup.cy.js
@@ -8,20 +8,13 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 let number = parseInt(Math.random()*10000000, 10);
 let phone_number = '09034' + number;
-let CategoryNames = 'QaLtd' + phone_number;
-let BusinessCompanyName = 'Bunmeey_Co.Ltd' + phone_number;
 let email = phone_number + '@yopmail.com';
-let accFirstname = 'Fagbohungbe';
-let accLastname = 'Oluwabunmi';
-
 
 describe("SIGN UP", function () {
     beforeEach(function () {
           cy.intercept('POST','/api/v1/user/signUp').as('getEndpoint')
         cy.visit("/")
-
     });
-
 
 it("VALID SIGNUP - I Should be able to register a new user", function () {
     cy.get(Login.loginBtn).click()
@@ -33,24 +26,11 @@ it("VALID SIGNUP - I Should be able to register a new user", function () {
     cy.contains('An OTP would be sent to the email address you provide.').should('be.visible')
     cy.get(Signup.Password).type("TobiBum1@")
     cy.get(Signup.Confirmpassword).type("TobiBum1@")
-
-    // cy.intercept('POST',"https://plug-insure-auth.vercel.app/api/v1/user/signUp", (req)=>{
-    //     req.reply((res) =>{
-    //         const responseBody = res.body;
-    //         cy.log(responseBody)
-    //         res.send(responseBody)
-    //     })
-    // }).as('myRequest')
-
     cy.get(Signup.Code).click()
-    cy.wait('@getEndpoint')
-    cy.get('@getEndpoint').then((interception) =>{
-        const responseBody = interception.response.body 
-        cy.log(responseBody)
-    })
+
         
 })
-    it("INVALID SIGNUP - I Should not be able to register a new user ith invalid details", function () {
+    it("INVALID SIGNUP - I Should not be able to register a new user with invalid details", function () {
         cy.get(Login.loginBtn).click()
         cy.get(Login.createAccount).click()
         cy.contains('Get Better insurance for Less.').should('be.visible')
